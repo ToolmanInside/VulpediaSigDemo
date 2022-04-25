a42fc84893d54a85b891d03f8dde5b4ae9c26c8b
Jiaming <yejiaming20@126.com>
2022-04-25 15:16:32 +0800

    update APTED distance

--- clustering.py
+++ clustering.py
@@ -1,66 +1,67 @@
 import os
 from ast_helper import AstHelper
 from solidity_compiler import SolidityCompiler
-from apted import APTED
-from collections import ChainMap
-from functools import reduce
-
-def edt_distance(tree1, tree2):
-    apted = APTED(tree1, tree2)
-    ted = apted.compute_edit_distance()
-    return ted
+from apted import APTED, helpers
+import numpy as np
 
 class Clustering(object):
     def __init__(self, target_folder_path):
         self.target_folder_path = target_folder_path
-        self.ast_helper = AstHelper(self.target_folder_path, input_type = "solidity", remap = "", allow_paths = "")
-        contracts = [x[0].split(":")[-1] for x in SolidityCompiler(self.target_folder_path).output()]
-        normalized_trees = self.normalization(contracts)
+        self.file_list = os.listdir(self.target_folder_path)
+        self.normalized_trees = list()
+        for ff in self.file_list:
+            file_path = os.path.join(self.target_folder_path, ff)
+            ast = AstHelper(file_path, input_type = "solidity", remap = "", allow_paths = "")
+            contracts = [x[0].split(":")[-1] for x in SolidityCompiler(file_path).output()]
+            self.normalized_trees.append(self.normalization(ast, contracts))
+        self.distanceMat = self.computeEdtDist(self.normalized_trees)
+        print(self.distanceMat)
 
-    def normalization(self, contracts):
+    def edt_distance(self, tree1, tree2):
+        apted = APTED(tree1, tree2)
+        ted = apted.compute_edit_distance()
+        return ted
+
+    def computeEdtDist(self, trees):
+        length = len(trees)
+        mat = np.zeros((length, length))
+        for i in range(length):
+            for j in range(length):
+                dist = self.edt_distance(trees[i][0], trees[j][0])
+                mat[i][j] = dist
+        return mat
+
+    def normalization(self, ast_helper, contracts):
         trees = list()
         for contract in contracts:
-            tree = self.ast_helper.get_func(contract)
-            output_str = ""
+            tree = ast_helper.get_func(contract)
             if tree == {}:
                 continue
-            flattree = self.flatmap(tree)
-            print(flattree)
-            # print(stable_str)
-        return contracts
+            trees.append(helpers.Tree.from_text(self.transform(tree)))
+        return trees
 
-    def _flatmap(self, tree, stable_str):
-        for k,v in tree.items():
-            # stable_str = stable_str + "{" + str(k)
-            if isinstance(v, dict):
-                stable_str = stable_str + "{" + str(k)
-                stable_str = self._flatmap(v, stable_str)
-            elif isinstance(v, list):
-                stable_str = stable_str + "{" + str(k)
-                stable_str = self._flatlist(v, stable_str)
-            else:
-                stable_str = "{" + stable_str + str(v) + "}"
-                return stable_str
+    def transform_recusion(self, d):
+        if type(d) == type('s'):
+            return '{%s}' % d
+        if type(d) == type(1):
+            return '{%d}' % d
+        if type(d) == type({}):
+            s = ''
+            for key in d:
+                s += self.transform_recusion(d[key])
+            return '{%s}' % s
+        if type(d) == type([]):
+            s = ''
+            for key in d:
+                s += self.transform_recusion(key)
+            return '%s' % s
+        return ''
 
-    def _flatlist(self, tree, stable_str):
-        stable_str = stable_str + "{"
-        for e in tree:
-            if isinstance(e, dict):
-                self._flatmap(e, stable_str)
-            elif isinstance(e, list):
-                self._flatlist(e, stable_str)
-        stable_str = stable_str + "}"
-        return stable_str
-
-    def flatmap(self, tree):
-        stable_str = ""
-        if isinstance(tree, dict):
-            stable_str = self._flatmap(tree, stable_str)
-        elif isinstance(tree, list):
-            stable_str = self._flatlist(tree, stable_str)
-        else:
-            stable_str = stable_str + "}"
-        return stable_str
+    def transform(self, d):
+        s = ''
+        for key in d:
+            s += '{' + key + self.transform_recusion(d[key]) + '}'
+        return '{root' + s + '}'
 
 if __name__ == "__main__":
-    cluster = Clustering(os.path.join("demo_code", "com-1.sol"))
+    cluster = Clustering(os.path.join("demo_code"))
